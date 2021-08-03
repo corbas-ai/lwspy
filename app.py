d4ac5d2a81a2b8da77616574a9a0f19f23875153
Anton Idukov <idukov@crtc.spb.ru>
2021-08-03 18:16:06 +0300

    init

---
+++ app.py
@@ -0,0 +1,37 @@
+from starlette.applications import Starlette
+from starlette.responses import PlainTextResponse
+from starlette.routing import Route, Mount, WebSocketRoute
+from starlette.staticfiles import StaticFiles
+
+
+def homepage(request):
+    return PlainTextResponse('Hello, world!')
+
+
+def user_me(request):
+    username = "John Doe"
+    return PlainTextResponse('Hello, %s!' % username)
+
+def user(request):
+    username = request.path_params['username']
+    return PlainTextResponse('Hello, %s!' % username)
+
+async def websocket_endpoint(websocket):
+    await websocket.accept()
+    while True:
+        msg = await websocket.receive_text()
+        await websocket.send_text(f'Hello, {{{msg}}}, websocket!')
+    await websocket.close()
+
+def startup():
+    print('Ready to go')
+
+
+routes = [
+    Route('/user/me', user_me),
+    Route('/user/{username}', user),
+    WebSocketRoute('/ws', websocket_endpoint),
+    Mount('/', StaticFiles(directory="mount-origin", html=True, check_dir=True)),
+]
+
+app = Starlette(debug=True, routes=routes, on_startup=[startup])
